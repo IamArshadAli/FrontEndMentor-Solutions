67bdeab8d2745e1a271a7ba0988e15c93eea0758
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-21 23:51:15 +0530

    2-5 assets

---
+++ 2-5-interactive-card-details-form-main/src/assets/images/index.js
@@ -0,0 +1,15 @@
+import cardBackBg from "./bg-card-back.png";
+import cardFrontBg from "./bg-card-front.png";
+import mainDesktopBg from "./bg-main-desktop.png";
+import mainMobileBg from "./bg-main-mobile.png";
+import cardLogo from "./card-logo.svg";
+import completeIcon from "./icon-complete.svg";
+
+export {
+  cardBackBg,
+  cardFrontBg,
+  mainDesktopBg,
+  mainMobileBg,
+  cardLogo,
+  completeIcon,
+};
