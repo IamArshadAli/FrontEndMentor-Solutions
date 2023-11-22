302fa09f95fd62b20df6d4ec2a006383a77fad3b
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-22 20:23:05 +0530

    2-5 assets

--- 2-5-interactive-card-details-form-main/src/assets/images/index.js
+++ 2-5-interactive-card-details-form-main/src/assets/images/index.js
@@ -5,6 +5,10 @@ import mainMobileBg from "./bg-main-mobile.png";
 import cardLogo from "./card-logo.svg";
 import completeIcon from "./icon-complete.svg";
 
+import amexCardIcon from "./amex-card.png";
+import masterCardIcon from "./master-card.png";
+import visaCardIcon from "./visa-card.png";
+
 export {
   cardBackBg,
   cardFrontBg,
@@ -12,4 +16,7 @@ export {
   mainMobileBg,
   cardLogo,
   completeIcon,
+  amexCardIcon,
+  masterCardIcon,
+  visaCardIcon,
 };
