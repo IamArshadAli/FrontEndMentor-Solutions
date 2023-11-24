0a8086baff53db469538b2fa95c3727a1059df12
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-24 21:31:23 +0530

    2-7 assets

--- 2-7-intro-section-with-dropdown-navigation/src/assets/images/index.js
+++ 2-7-intro-section-with-dropdown-navigation/src/assets/images/index.js
@@ -1,11 +1,12 @@
 import audiophileClient from "./client-audiophile.svg";
 import databizClient from "./client-databiz.svg";
-import markerClient from "./client-marker.svg";
+import makerClient from "./client-maker.svg";
 import meetClient from "./client-meet.svg";
 import arrowDownIcon from "./icon-arrow-down.svg";
 import arrowUpIcon from "./icon-arrow-up.svg";
 import calendarIcon from "./icon-calendar.svg";
 import closeMenuIcon from "./icon-close-menu.svg";
+import menuIcon from "./icon-menu.svg";
 import planningIcon from "./icon-planning.svg";
 import remindersIcon from "./icon-reminders.svg";
 import todoIcon from "./icon-todo.svg";
@@ -16,12 +17,13 @@ import logo from "./logo.svg";
 export {
   audiophileClient,
   databizClient,
-  markerClient,
+  makerClient,
   meetClient,
   arrowDownIcon,
   arrowUpIcon,
   calendarIcon,
   closeMenuIcon,
+  menuIcon,
   planningIcon,
   remindersIcon,
   todoIcon,
