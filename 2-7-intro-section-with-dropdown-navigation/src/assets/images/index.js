395c29186df0a46131a3e1205d3b9e2f76aef9f0
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-24 10:53:46 +0530

    2-7 assets

---
+++ 2-7-intro-section-with-dropdown-navigation/src/assets/images/index.js
@@ -0,0 +1,31 @@
+import audiophileClient from "./client-audiophile.svg";
+import databizClient from "./client-databiz.svg";
+import markerClient from "./client-marker.svg";
+import meetClient from "./client-meet.svg";
+import arrowDownIcon from "./icon-arrow-down.svg";
+import arrowUpIcon from "./icon-arrow-up.svg";
+import calendarIcon from "./icon-calendar.svg";
+import closeMenuIcon from "./icon-close-menu.svg";
+import planningIcon from "./icon-planning.svg";
+import remindersIcon from "./icon-reminders.svg";
+import todoIcon from "./icon-todo.svg";
+import heroDesktopImage from "./image-hero-desktop.png";
+import heroMobileImage from "./image-hero-mobile.png";
+import logo from "./logo.svg";
+
+export {
+  audiophileClient,
+  databizClient,
+  markerClient,
+  meetClient,
+  arrowDownIcon,
+  arrowUpIcon,
+  calendarIcon,
+  closeMenuIcon,
+  planningIcon,
+  remindersIcon,
+  todoIcon,
+  heroDesktopImage,
+  heroMobileImage,
+  logo,
+};
