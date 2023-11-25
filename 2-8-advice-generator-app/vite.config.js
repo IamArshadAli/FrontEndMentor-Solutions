831590315097fadc81edcf704509071a74e15516
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-25 12:05:33 +0530

    2-8 advice-generator-app

---
+++ 2-8-advice-generator-app/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/FrontEndMentor-Solutions/2-8-advice-generator-app/'
+})
